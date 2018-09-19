88cbfa6c53535e5ea847526ac7a323290d2d943c
sunjakim_mac <sunjakim_mac@Hayoung-ui-MacBook-Pro.local>
2018-09-20 03:59:39 +0900

    first commit

---
+++ week2/yourStory.py
@@ -0,0 +1,22 @@
+print("This is your story")
+print("Answer the questions below to play")
+
+
+
+name1 = raw_input("Enter your name")
+place = raw_input ("What is your favorie place to relax?")
+name2 = raw_input ("Best friend's name? ")
+animal1=raw_input ("Animal that scares the most")
+address1 = raw_input ("What is your address?")
+
+yourStory = "Long time ago " + "there was a rabbit named " + name1 + \
+". One day"+ "," + name1  + " decided to" + " visit " + place + \
+". The rabbit "+"finds out that "+ place + " was disappeared. " + \
+name1 + " was so disappointed."+ \
+" The way back home, "+ name1 + " met "+ name2 + \
+name2 + " told the rabbit that he/she is a rabbit too." + \
+" However,"+ name1 + " finds out that " + name2 + " is not an rabbit " + \
+name2 + " was " + animal1 + "!!!!!!!" \
+" The rabbit" + " ran into the rabbit hole" + " called " + address1 + "."
+
+print(yourStory)
